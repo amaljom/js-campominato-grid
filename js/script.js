de50908cc50fa1fefa5d3ae6da3bbd0d32ba185b
amaljom <amaljom99@gmail.com>
2022-06-23 17:39:20 +0200

    aggiunto un event listener che colora la cella e fa comparire il numero in console

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 //  ! dobbiamo generare automaticamente cento caselle dentro il container presente nel html
 const container = document.getElementById('containder-grid');
-
+// ! inserisco un array vuoto al di fuori delle funzioni, che segna i numeri gia usati
 const blackList=[];
 // ! FUNZIONE CHE GENERA UN NUOVO QUADRATO
 function createElement(){
@@ -23,14 +23,28 @@ function createNumber(usedNumbers){
 
 // * richiamo la funzione per genereare quadrati
 for (let index = 0; index < 100; index++) {
+
     // ? RICHIAMO DELLA FUNZIONEPER GENERARE LE BOX
     const newGridBox = createElement();
 
     // ? RICHIAMO DELLA FUNZIONE PER GENERARE I NUMERI
     
     const gridNumber= createNumber(blackList);
-
     newGridBox.innerHTML=gridNumber;
     blackList.push(gridNumber);
+
     container.append(newGridBox);
+
+    newGridBox.addEventListener('click', function(){
+        newGridBox.classList.toggle('bg');
+        console.log(newGridBox.innerText);
+        });
+    
 }
+// const eachBox= document.querySelector('.box-container', '.new-grid');
+
+//     eachBox.addEventListener('click', function(){
+//     newGridBox.classList.add('bg');
+
+//     container.append(eachBox);
+//     });
